f0d4a479ae035ced2ee2822c42bc50ef4607969a
Nguyễn Hoàng Vũ <hoangvu22062001@gmail.com>
2023-08-04 15:26:54 +0700

    routes and  controller

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+const newsRouter = require("./new");
+const sitesRouter = require("./site");
+
+function route(app) {
+  app.use("/news", newsRouter);
+
+  app.use("/", sitesRouter);
+}
+
+module.exports = route;
